d4abbcad3c166f8644e6a693656024591f24ca36
psy1064 <psy1064@naver.com>
2018-07-28 14:22:50 +0000

    report card : Show the sum of each grade
    Signed-off-by: psy1064 <psy1064@naver.com>

--- report_card.c
+++ report_card.c
@@ -3,6 +3,7 @@
 int main(void)
 {
 	int kor, eng, math;
+	int sum = 80+100+90;
 	math = 80; 
 	eng = 100;
 	kor = 90;
@@ -11,6 +12,7 @@ int main(void)
 	printf("Korean : %d\n",kor);
 	printf("English : %d\n",eng);
 	printf("Math : %d\n",math);
+	printf("Sum : %d\n",sum);
 
 	return 0;
 }
