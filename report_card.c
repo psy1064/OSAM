aec683e3b51750ddb1d4e13716196314e17e1dce
psy1064 <psy1064@naver.com>
2018-07-28 14:21:20 +0000

    report card : Print grades of each subject

--- report_card.c
+++ report_card.c
@@ -2,6 +2,15 @@
 
 int main(void)
 {
+	int kor, eng, math;
+	math = 80; 
+	eng = 100;
+	kor = 90;
+
 	printf("This Program print report card.\n");
+	printf("Korean : %d\n",kor);
+	printf("English : %d\n",eng);
+	printf("Math : %d\n",math);
+
 	return 0;
 }
