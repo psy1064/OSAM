7770cd9c85d2a7de5d26c5add9c142b8e8fe4d2b
psy1064 <psy1064@naver.com>
2018-08-06 13:44:46 +0000

    calculation example

---
+++ python/calculation.py
@@ -0,0 +1,25 @@
+add = 10 + 20  # add 에 10 + 20
+print(add)    # add 출력
+
+print(2*2*2*2*2*2*2*2*2*2)
+print(2**10)  # 2^10 값을 표현하는 두가지 방법
+
+compA = 20
+compB = 30
+print(compA < compB)  # compA가 compB보다 작은지 확인
+print(compA > compB)  # compA가 compb보다 큰 지 확인
+
+trueA = 1
+trueB = 1
+falseA = 0
+falseB = 0
+
+print(not trueA)  # not 연산
+print(trueA and trueB)  # and 연산
+
+print(1204-48)  # 1204-48 출력
+
+so = 1000
+cake = 15000
+pat = 800
+print(so*17+cake*3+pat*8)  # 소보루빵 17개, 케이크 3개 팥빵 8개의 매출
