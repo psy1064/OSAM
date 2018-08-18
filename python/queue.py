6c21543ebf968da0289d42da01e6b6a761874c6b
psy1064 <psy1064@naver.com>
2018-08-18 13:27:10 +0000

    Implement queue using list + add method

--- python/queue.py
+++ python/queue.py
@@ -1,6 +1,9 @@
 # Queue example
 # Queue is FIFO(first in first out) construction
 
+def printqueue(_list):
+	for x in range(len(_list)):
+		print("순서 = ", x+1, "데이터 = ", _list[x])
 queue = []
 
 while(1):
@@ -11,3 +14,5 @@ while(1):
 
 for x in range(len(queue)):
 	print("순서 = ", x+1, "데이터 = ", queue[x])
+
+printqueue(queue)
